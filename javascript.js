07e5fe2671dc07e027e8f59244bb2bc68ed46343
Darren Baker <d.baker089@gmail.com>
2022-07-29 10:26:27 -0400

    Fix playerChoice function

--- javascript.js
+++ javascript.js
@@ -1,34 +1,51 @@
 //set choices
 
 const choices = ['rock', 'paper', 'scissors'];
+let compChoice;
+let playerChoice;
+let compScore;
+let playerScore;
 
-//computer choice
+//function game()
 
-function getCompChoice() {
-    let compChoice = (choices[(Math.floor(Math.random() * (2 + 1)))]);
-    console.log(compChoice);
+//function playRound(compChoice, playerChoice)
+
+function playRound(compChoice, playerChoice) {
+
+    //computer choice
+
+    function getCompChoice() {
+        compChoice = (choices[(Math.floor(Math.random() * 3))]);
+        console.log(compChoice);
+    }
+
+    getCompChoice();
+
+
+    // player choice
+
+    function getPlayerChoice() {
+        playerChoice = prompt("Choose Rock, Paper, or Scissors");
+        console.log(playerChoice);
+        // return playerChoice;
+    }
+
+    getPlayerChoice();
+
+
+    //compare choices
+
+    if (playerChoice == compChoice) {
+        var element = document.getElementById("results");
+        element.innerHTML = "Tie";
+    } else if (playerChoice == 'rock' && compChoice == 'scissors' || playerChoice == 'paper' && compChoice == 'rock' || playerChoice == "scissors" && compChoice == 'paper') {
+        var element = document.getElementById("results");
+        element.innerHTML = "Player Wins";
+    } else {
+        var element = document.getElementById("results");
+        element.innerHTML = "Computer Wins";
+    }
+
 }
 
-let compChoice = getCompChoice();
-
-//player choice
-
-function getPlayerChoice() {
-    let playerChoice = prompt("Choose Rock, Paper, or Scissors");
-    console.log(playerChoice);
-}
-
-let playerChoice = getPlayerChoice();
-
-//compare choices
-
-if (playerChoice == compChoice) {
-    var element = document.getElementById("results");
-    element.innerHTML = "Tie";
-} else if (playerChoice == 'rock' && compChoice == 'scissors' || playerChoice == 'paper' && compChoice == 'rock' || playerChoice == "scissors" && compChoice == 'paper') {
-    var element = document.getElementById("results");
-    element.innerHTML = "Player Wins";
-} else {
-    var element = document.getElementById("results");
-    element.innerHTML = "Computer Wins";
-}
+playRound(compChoice, playerChoice);
