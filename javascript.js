51fe6857080495d9efe6655d5d57d6145a24c033
Darren Baker <d.baker089@gmail.com>
2022-08-03 14:21:26 -0400

    prepped for gui

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-//set choices
+//set variables
 
 const choices = ['rock', 'paper', 'scissors'];
 let compChoice;
@@ -10,77 +10,55 @@ let gameCount = 0;
 //function game()
 
 function game() {
-    if (gameCount <= 5) {
+    while (gameCount < 5) {
+        playRound(compChoice, playerChoice);
+    }
+}
 
-        //function playRound(compChoice, playerChoice)
+function playRound(compChoice, playerChoice) {
 
-        function playRound(compChoice, playerChoice) {
+    //computer choice
 
-            //computer choice
+    function getCompChoice() {
+        compChoice = (choices[(Math.floor(Math.random() * 3))]);
+        console.log(`Computer: ${compChoice}`);
+    }
 
-            function getCompChoice() {
-                compChoice = (choices[(Math.floor(Math.random() * 3))]);
-                console.log(`Computer: ${compChoice}`);
-            }
-
-            getCompChoice();
-
-
-            // player choice
-
-            function getPlayerChoice() {
-                playerChoice = prompt("Choose Rock, Paper, or Scissors");
-                console.log(`Player: ${playerChoice}`);
-            }
-
-            getPlayerChoice();
+    getCompChoice();
 
 
-            //compare choices
+    // player choice
 
-            if (playerChoice == compChoice) {
-                var element = document.getElementById("results");
-                element.innerHTML = "Tie";
-                gameCount++;
-                console.log(`Player Score: ${playerScore}`);
-                console.log(`Computer Score: ${compScore}`);
-                console.log(`Game Count: ${gameCount}`);
-            } else if (playerChoice == 'rock' && compChoice == 'scissors' || playerChoice == 'paper' && compChoice == 'rock' || playerChoice == "scissors" && compChoice == 'paper') {
-                var element = document.getElementById("results");
-                element.innerHTML = "Player Wins";
-                playerScore++;
-                gameCount++;
-                console.log(`Player Score: ${playerScore}`);
-                console.log(`Computer Score: ${compScore}`);
-                console.log(`Game Count: ${gameCount}`);
+    function getPlayerChoice() {
+        playerChoice = prompt("Choose Rock, Paper, or Scissors");
+        console.log(`Player: ${playerChoice}`);
+    }
 
-            } else {
-                var element = document.getElementById("results");
-                element.innerHTML = "Computer Wins";
-                compScore++;
-                gameCount++;
-                console.log(`Player Score: ${playerScore}`);
-                console.log(`Computer Score: ${compScore}`);
-                console.log(`Game Count: ${gameCount}`);
-
-            }
+    getPlayerChoice();
 
 
-            if (playerScore === 3) {
-                var element = document.getElementById("results");
-                element.innerHTML = "You won the game!";
-            } else if (compScore === 3) {
-                var element = document.getElementById("results");
-                element.innerHTML = "The computer won the game!";
-            } else {
-                playRound(compChoice, playerChoice);
-            }
-        } else {
-            playerScore = 0;
-            compScore = 0;
-            gameCount = 0
-            playRound(compChoice, playerChoice);
-        }
+    //compare choices
+
+    if (playerChoice == compChoice) {
+        gameCount++;
+        console.log(`---Result: Tie---`);
+        console.log(`Player Score: ${playerScore}`);
+        console.log(`Computer Score: ${compScore}`);
+        console.log(`===Game Count: ${gameCount}===`);
+    } else if (playerChoice == 'rock' && compChoice == 'scissors' || playerChoice == 'paper' && compChoice == 'rock' || playerChoice == "scissors" && compChoice == 'paper') {
+        playerScore++;
+        gameCount++;
+        console.log(`---Result: Player Wins---`);
+        console.log(`Player Score: ${playerScore}`);
+        console.log(`Computer Score: ${compScore}`);
+        console.log(`===Game Count: ${gameCount}===`);
+    } else {
+        compScore++;
+        gameCount++;
+        console.log(`---Result: Computer Wins---`);
+        console.log(`Player Score: ${playerScore}`);
+        console.log(`Computer Score: ${compScore}`);
+        console.log(`===Game Count: ${gameCount}===`);
     }
 }
 
