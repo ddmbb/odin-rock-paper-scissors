4c6b665d21858743cb315074d7c6787bd801b1f0
Darren Baker <d.baker089@gmail.com>
2022-08-05 14:11:29 -0400

    Add GUI

--- javascript.js
+++ javascript.js
@@ -6,64 +6,87 @@ let playerChoice;
 let compScore = 0;
 let playerScore = 0;
 let gameCount = 0;
+const buttons = document.querySelectorAll('button');
 
-//function game()
+//add event listeners on click + set playerChoice (?querySelectorAll?)
 
-function game() {
-    while (gameCount < 5) {
-        playRound(compChoice, playerChoice);
-    }
+rock.addEventListener('click', function () {
+    playerChoice = 'rock';
+    playRound();
+});
+
+paper.addEventListener('click', function () {
+    playerChoice = 'paper';
+    playRound();
+});
+
+scissors.addEventListener('click', function () {
+    playerChoice = 'scissors';
+    playRound();
+});
+
+hideReset();
+
+
+//get computer choice *******************************
+
+function getCompChoice() {
+    compChoice = (choices[(Math.floor(Math.random() * 3))]);
 }
 
-function playRound(compChoice, playerChoice) {
+//play round
 
-    // player choice
-
-    function getPlayerChoice() {
-        playerChoice = prompt("Choose Rock, Paper, or Scissors");
-        console.log(`Player: ${playerChoice}`);
-    }
-
-    getPlayerChoice();
-
-    //computer choice
-
-    function getCompChoice() {
-        compChoice = (choices[(Math.floor(Math.random() * 3))]);
-        console.log(`Computer: ${compChoice}`);
-    }
-
+function playRound() {
     getCompChoice();
-
-    //compare choices
-
-    function compare() {
-        if (playerChoice == compChoice) {
-            gameCount++;
-            console.log(`===Result: Tie===`);
-            console.log(`Game Count: ${gameCount}`);
-            console.log(`Player Score: ${playerScore}`);
-            console.log(`Computer Score: ${compScore}`);
-            console.log('---Next Round---');
-        } else if (playerChoice == 'rock' && compChoice == 'scissors' || playerChoice == 'paper' && compChoice == 'rock' || playerChoice == "scissors" && compChoice == 'paper') {
-            playerScore++;
-            gameCount++;
-            console.log(`===Result: Player Wins===`);
-            console.log(`Game Count: ${gameCount}`);
-            console.log(`Player Score: ${playerScore}`);
-            console.log(`Computer Score: ${compScore}`);
-            console.log('---Next Round---');
-        } else {
-            compScore++;
-            gameCount++;
-            console.log(`===Result: Computer Wins===`);
-            console.log(`Game Count: ${gameCount}`);
-            console.log(`Player Score: ${playerScore}`);
-            console.log(`Computer Score: ${compScore}`);
-            console.log('---Next Round---');
-        }
-    }
     compare();
+    endGame();
 }
 
-game();
+//compare choices *********************
+
+function compare() {
+    if (playerChoice == compChoice) {
+        gameCount++;
+        results.textContent = `Player: ${playerChoice} ties Computer: ${compChoice}`
+    } else if (playerChoice == 'rock' && compChoice == 'scissors' || playerChoice == 'paper' && compChoice == 'rock' || playerChoice == "scissors" && compChoice == 'paper') {
+        playerScore++;
+        gameCount++;
+        results.textContent = `Player: ${playerChoice} beats Computer: ${compChoice}`;
+    } else {
+        compScore++;
+        gameCount++;
+        results.textContent = `Player: ${playerChoice} loses to Computer: ${compChoice}`;
+    }
+}
+
+//end the game
+
+function endGame() {
+    if (playerScore === 3) {
+        results.textContent = `PLAYER WINS ${playerScore} to ${compScore}`;
+        for (const button of buttons) {
+            button.classList.toggle('hide');
+        };
+    }
+    if (compScore === 3) {
+        results.textContent = `COMPUTER WINS ${compScore} to ${playerScore}`;
+        for (const button of buttons) {
+            button.classList.toggle('hide');
+        };
+    }
+}
+
+//reset
+
+reset.addEventListener('click', function () {
+    playerScore = 0;
+    compScore = 0;
+    gameCount = 0;
+    for (const button of buttons) {
+        button.classList.toggle('hide');
+    };
+});
+
+function hideReset() {
+    reset.classList.toggle('hide');
+}
